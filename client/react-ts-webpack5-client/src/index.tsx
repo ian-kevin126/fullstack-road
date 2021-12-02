d9741a750bad07e451d4b2c59f259df0650f247d
liaojiayun <liaojiayun@moresales.cn>
2021-12-02 18:18:54 +0800

    feat：初始化项目结构

--- client/react-ts-webpack5-client/src/index.tsx
+++ client/react-ts-webpack5-client/src/index.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssDemo from '@/pages/CssDemo';
-import AntdDemo from '@/pages/AntdDemo';
+import App from './App';
 import '@/index.less';
-// import East_White from '@/assets/East_White.jpg';
-
-const App = () => {
-  return (
-    <div>
-      <CssDemo />
-      <h1>My React and TypeScript App!</h1>
-      <AntdDemo />
-      {/* <img src={East_White} alt="" /> */}
-    </div>
-  );
-};
 
 ReactDOM.render(
   <React.StrictMode>
